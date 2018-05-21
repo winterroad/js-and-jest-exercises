e26350870aa172e17896998ceaa2a3ccb46bd294
winterroad <ullakaija.talvitie@gmail.com>
2018-05-21 21:04:46 +0300

    Feat: added alternative way to capitalize.

--- 08-capitalize/index.js
+++ 08-capitalize/index.js
@@ -1,3 +1,4 @@
+
 function capitalize(str) {
 
     let strArr = str.split(' ');
@@ -10,4 +11,22 @@ function capitalize(str) {
     
 }
 
+
+/*
+function capitalize(str) {
+
+    let capitalized = str[0].toUpperCase();
+
+    for(let i = 1; i < str.length; i++) {
+        if(str[i-1] === " ") {
+            capitalized += str[i].toUpperCase();
+        } else {
+            capitalized += str[i];
+        }
+    }
+
+    return capitalized;
+}
+
+*/
 module.exports = capitalize;
